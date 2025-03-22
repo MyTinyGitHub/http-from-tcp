2eedb0d442cfbc55536432e2f57a3224bda447e5
Dominik Pall <dominik.pall.eu@gmail.com>
2025-03-22 17:59:06 +0100

    Update parsing method

---
+++ internal/headers/headers_test.go
@@ -0,0 +1,102 @@
+package headers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestValidSingleHeader(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+}
+
+func TestInvalidSignSingleHeader(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("H@st: localhost:42069\r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+}
+
+func TestValidSingleHeaderWithExtraWhiteSpace(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("   Host: localhost:42069   \r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, 29, n)
+	assert.False(t, done)
+}
+
+func TestValidTwoSameHeadersSpace(t *testing.T) {
+	headers := NewHeaders()
+  data := []byte("Host: localhost:42069\r\nHost: localHoest:69\r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+
+  data = data[n:]
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069, localHoest:69", headers["host"])
+	assert.Equal(t, 21, n)
+	assert.False(t, done)
+}
+
+func TestTwoHeaders(t *testing.T) {
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\n Test: testtestst\r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+
+  data = data[n:]
+  n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "testtestst", headers["test"])
+	assert.Equal(t, 19, n)
+	assert.False(t, done)
+
+  n, done, err = headers.Parse(data[n:])
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, 2, n)
+	assert.True(t, done)
+}
+
+func TestInvalidSpacingHeader(t *testing.T) {
+  headers := NewHeaders()
+  data := []byte("       Host : localhost:42069       \r\n\r\n")
+  n, done, err := headers.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+}
+
+func TestCRLFAtTheBegginingOfLine(t *testing.T) {
+  // Test: CRLF at the begginig of line
+  headers := NewHeaders()
+  data := []byte("\r\n\r\n")
+  n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	assert.Equal(t, 2, n)
+	assert.True(t, done)
+}
